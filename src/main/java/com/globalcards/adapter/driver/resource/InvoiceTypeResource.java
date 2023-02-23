b8b7cb8e8710f3778564b028a332e28de544c489
Reis <lucas.barbosa.p.reis@accenture.com>
2023-02-23 19:30:06 -0300

    feat: criado projeto producer

--- src/main/java/com/globalcards/adapter/driver/resource/InvoiceTypeResource.java
+++ src/main/java/com/globalcards/adapter/driver/resource/InvoiceTypeResource.java
@@ -1,7 +1,6 @@
 package com.globalcards.adapter.driver.resource;
 
 import com.globalcards.adapter.infrastructure.entity.InvoiceType;
-import com.globalcards.domain.port.IInvoiceTypeService;
 import io.smallrye.mutiny.Uni;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.microprofile.reactive.messaging.Channel;
@@ -13,7 +12,6 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
-import java.util.UUID;
 
 
 @Path("invoice-type")
@@ -23,40 +21,8 @@ import java.util.UUID;
 @Slf4j
 public class InvoiceTypeResource {
 
-    @Channel("sends")
+    @Channel("invoice-requests")
     Emitter<String> invoyceTypeRequestEmitter;
-
-    @Inject
-    IInvoiceTypeService invoiceTypeService;
-
-    @GET
-    public Uni<List<InvoiceType>> get() {
-        return invoiceTypeService.getAll();
-    }
-
-    @GET
-    @Path("{id}")
-    public Uni<InvoiceType> getById(Long id) {
-        return invoiceTypeService.getById(id);
-    }
-
-    @PUT
-    @Path("{id}")
-    public Uni<Response> update(Long id, InvoiceType invoiceType) {
-        return invoiceTypeService.update(id, invoiceType );
-    }
-
-    @POST
-    public Uni<Response> create(InvoiceType invoiceType) {
-        return invoiceTypeService.save(invoiceType);
-    }
-
-    @DELETE
-    @Path("{id}")
-    public Uni<Response> delete(Long id) {
-        return invoiceTypeService.delete(id);
-    }
-
 
     @POST
     @Path("/emit")
